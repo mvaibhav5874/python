48b0bccb78e0ca5dd4a5616bbd2ce535f40a8dda
python <mvaibhav5874@gmail.com>
2024-03-11 11:12:52 +0530

    Merge remote-tracking branch 'origin/master'

---
+++ python/mst.py
@@ -0,0 +1,16 @@
+name = input("Enter student name: ")
+class_name = input("Enter your class: ")
+section = input("Enter your section: ")
+marks = float(input("Enter your marks: "))
+if marks >= 90:
+    print('he got a')
+if marks >= 80:
+    print('he got a')
+else:
+    print('he got b')
+if marks >= 90:
+    print('he got a')
+elif marks >= 80:
+    print('he got b')
+else:
+    print('he got c')
