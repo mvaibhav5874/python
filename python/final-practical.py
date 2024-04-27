9930ecad096b768e798f650cde237ef9b43d426c
python <mvaibhav5874@gmail.com>
2024-04-27 12:26:49 +0530

    Merge remote-tracking branch 'origin/master'

--- python/final-practical.py
+++ python/final-practical.py
@@ -6,14 +6,14 @@ a = (1,)
 print(a)
 print(type(a))
 
+merge= a+t
+print(merge)
+print(type(merge))
+
 li1 = [1, 3, 5, 7, 9]
 z = tuple(li1)
 print(z)
 print(type(z))
-
-merge= a+t
-print(merge)
-print(type(merge))
 
 repeated_tuple = (19, 29, 39) * 2
 print("Repeated tuple:", repeated_tuple)
@@ -21,11 +21,11 @@ print("Repeated tuple:", repeated_tuple)
 d = ((1, 3, 5), (2, 4, 6))
 print("Nested Tuple:", d)
 
-print(len(t))
+tuple2 = merge[1:6]
+print(tuple2)
 
 print("count of tuple", merge.count(2))
 
-tuple2 = merge[1:6]
-print(tuple2)
+print(len(t))
 
 del (tuple2)
