cfe1663fa2572210b2c738474b21c1e73fc799be
mvaibhav5874 <mvaibhav5874@gmail.com>
2024-12-12 10:53:56 +0530

    organisation commit

--- DSA/ARRAY_COUNT.py
+++ DSA/ARRAY_COUNT.py
@@ -1,3 +1,8 @@
+"""QUESTION :-Given a sorted array, arr[] and a number target, you need to find the number of occurrences of target in arr[].
+Examples :
+Input: arr[] = [1, 1, 2, 2, 2, 2, 3], target = 2
+Output: 4
+Explanation: target = 2 occurs 4 times in the given array so the output is 4."""
 class Solution:
     def countFreq(self, arr, target):
         count = 0
